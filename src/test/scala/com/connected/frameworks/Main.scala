83e613c87fb15e0555f6c64a5da52d80bf30d321
gopal <gopal.tiwari2012@gmail.com>
2019-04-29 18:07:31 +0530

    comment added

--- src/test/scala/com/connected/frameworks/Main.scala
+++ src/test/scala/com/connected/frameworks/Main.scala
@@ -6,26 +6,27 @@ import org.apache.spark.sql.SparkSession
 object Main extends SparkDatasetTester {
   def main(args: Array[String]): Unit = {
 
+    // Create spark session
     val spark = SparkSession.builder
       .appName("Parser").master("local[2]")
       .getOrCreate()
     import spark.implicits._
 
-
+    // Create result df
     val resultDF = Seq(
       (8, "bat"),
       (64, "mouse"),
       (-27, "horse")
     ).toDF("number", "word")
 
-
+    // Create expected df
     val expectedDF = Seq(
       (8, "bat"),
       (64, "mouse"),
       (-3, "horse")
     ).toDF("number", "word")
 
-
+    //check equality
     assertDatasetEquality(resultDF, expectedDF, orderedComparision = false)
   }
 }
