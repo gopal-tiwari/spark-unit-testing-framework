58996b9e68157bf363dca58aaa40687829593525
gopal <gopal.tiwari2012@gmail.com>
2019-06-01 21:46:17 +0530

    row count doc

--- src/test/scala/com/connected/frameworks/exceptions/SchemaMismatchException.scala
+++ src/test/scala/com/connected/frameworks/exceptions/SchemaMismatchException.scala
@@ -1,3 +1,7 @@
 package com.connected.frameworks.exceptions
 
+/**
+  * This class is used to throw custom message in case of table structure is not same.
+  * @param message [[String]] containing message to be shown with Exception.
+  */
 case class SchemaMismatchException(message: String) extends Exception(message)
