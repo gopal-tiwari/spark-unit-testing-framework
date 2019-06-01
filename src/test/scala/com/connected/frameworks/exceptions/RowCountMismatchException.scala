8bef5aae6479755cf122a54abdf6233c29135857
gopal <gopal.tiwari2012@gmail.com>
2019-06-01 21:47:12 +0530

    exception message reformatting

--- src/test/scala/com/connected/frameworks/exceptions/RowCountMismatchException.scala
+++ src/test/scala/com/connected/frameworks/exceptions/RowCountMismatchException.scala
@@ -2,6 +2,7 @@ package com.connected.frameworks.exceptions
 
 /**
   * This class is used to throw custom message in case of no of records are not equal.
+  *
   * @param message [[String]] containing message to be shown with Exception.
   */
 case class RowCountMismatchException(message: String) extends Exception(message)
